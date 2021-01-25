12860d59b6d57e1a7c2968681a8918cd36d85e70
Wojciech Budziak <steamvexlex@gmail.com>
2021-01-25 02:36:06 +0100

    add custom checkboxes

--- main.js
+++ main.js
@@ -21,8 +21,7 @@
         charsNumber = 11;
         let rotateDeg = 0;
 
-        const event = (e)=>{
-            console.log(e.target.value);
+        const slider = (e)=>{
             inputLength = e.target.value;
             inputLength = parseInt(inputLength);
             pswdlenght.textContent = `lenght ${e.target.value}`;
@@ -40,44 +39,6 @@
             charsNumber = inputLength;
             generatePassword();
         }
-        input.addEventListener('input', event);
-        checkLower.addEventListener('click', ()=>{
-            if(checkLower.checked == false){
-                chars.lowerCase = '';
-                generatePassword();
-            }else{
-                chars.lowerCase = 'qwertyuiopasdfghjklzxcvbnm';
-                generatePassword();
-            }
-        })
-        checkUpper.addEventListener('click', ()=>{
-            if(checkUpper.checked == false){
-                chars.upperCase = '';
-                generatePassword();
-            }else{
-                chars.upperCase = 'QWERTYUIOPASDFGHJKLZXCVBNM';
-                generatePassword();
-            }
-        })
-        checkSymbols.addEventListener('click', ()=>{
-            if(checkSymbols.checked == false){
-                chars.symbols = '';
-                generatePassword();
-            }else{
-                chars.symbols = '@#$%^&*()-_=+/?><';
-                generatePassword();
-            }
-        })
-        checkNumbers.addEventListener('click', ()=>{
-            if(checkNumbers.checked == false){
-                chars.numbers = '';
-                generatePassword();
-            }else{
-                chars.numbers = '1234567890';
-                generatePassword();
-            }
-        })
-       
         const generatePassword = ()=>{
             newPassword = '';
             pswdInput.value = "";
@@ -85,7 +46,6 @@
                 let generate = Math.floor(Math.random()* (chars.symbols.length + chars.lowerCase.length + chars.upperCase.length + chars.numbers.length
                 ));
                 newPassword += (chars.symbols + chars.lowerCase + chars.upperCase + chars.numbers)[generate];
-              
             }  
             if(chars.symbols === "" && chars.lowerCase === "" && chars.upperCase === "" && chars.numbers === ""){
                 return;
@@ -104,5 +64,62 @@
             document.execCommand("copy");
             alert("copied");
         }
-        copy.addEventListener('click', copyFunction)
-
+        copy.addEventListener('click', copyFunction);
+        input.addEventListener('input', slider);
+        
+        let lowerCheck = false;
+        checkLower.addEventListener('click', ()=>{
+            if(lowerCheck){
+                lowerCheck = !lowerCheck;
+                checkLower.classList.remove('unChecked');
+                chars.upperCase = 'qwertyuiopasdfghjklzxcvbnm';
+                generatePassword();
+            }else{
+                lowerCheck = !lowerCheck;
+                checkLower.classList.add('unChecked');
+                chars.lowerCase = '';
+                generatePassword();
+            }
+        })
+        let upperCheck = false;
+        checkUpper.addEventListener('click', ()=>{
+            if(upperCheck){
+                upperCheck = !upperCheck;
+                checkUpper.classList.remove('unChecked');
+                chars.upperCase = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+                generatePassword();
+            }else{
+                upperCheck = !upperCheck;
+                checkUpper.classList.add('unChecked');
+                chars.upperCase = '';
+                generatePassword();
+            }
+        })
+        let symbolsCheck = false;
+        checkSymbols.addEventListener('click', ()=>{
+            if(symbolsCheck){
+                symbolsCheck = !symbolsCheck;
+                checkSymbols.classList.remove('unChecked');
+                chars.symbols = '@#$%^&*()-_=+/?><';
+                generatePassword();
+            }else{
+                symbolsCheck = !symbolsCheck;
+                checkSymbols.classList.add('unChecked');
+                chars.symbols = '';
+                generatePassword();
+            }
+        })
+        let numbersCheck = false;
+        checkNumbers.addEventListener('click', ()=>{
+            if(numbersCheck){
+                numbersCheck = !numbersCheck;
+                checkNumbers.classList.remove('unChecked');
+                chars.numbers = '1234567890';
+                generatePassword();
+            }else{
+                numbersCheck = !numbersCheck;
+                checkNumbers.classList.add('unChecked');
+                chars.numbers = '';
+                generatePassword();
+            }
+        })
